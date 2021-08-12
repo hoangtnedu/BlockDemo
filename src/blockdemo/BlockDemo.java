4028b2d244511121015fa326820cda97395ff1c7
HoangTN <HoangTN@DESKTOP-H68FGFI>
2021-08-12 11:23:38 +0700

    Initial commit

--- src/blockdemo/BlockDemo.java
+++ src/blockdemo/BlockDemo.java
@@ -16,6 +16,13 @@ public class BlockDemo {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+          boolean condition = true;
+          if (condition) { // begin block 1
+               System.out.println("Condition is true.");
+          } // end block one
+          else { // begin block 2
+               System.out.println("Condition is false.");
+          } // end block 2 
     }
     
 }
